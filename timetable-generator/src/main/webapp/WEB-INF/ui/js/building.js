62681528eb0f367c9c31475f1950b5583fa66343
Angrytree <steainer@gmail.com>
2015-12-08 20:37:39 +0200

    edit

--- timetable-generator/src/main/webapp/WEB-INF/ui/js/building.js
+++ timetable-generator/src/main/webapp/WEB-INF/ui/js/building.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
             url:"resources/building",
             dataType:"json",
             success :  function(data){
-                $("#content").append("<div class='entity'>"+
+                $(".add").before("<div class='entity'>"+
                 "<div class='del-pane' >" +
                 "<input type='hidden' value="+data.id +" /> &#10006;"+
                 "</div>"+
@@ -26,35 +26,10 @@ $(document).ready(function(){
     }
 
 
-    function delBuilding(element){
-        console.log("on delete : ");
 
-        var id = element.parent().children(":first").val();
-        console.log("on delete : "+id);
-        $.ajax({
-            type:"DELETE",
-            url:"resources/building?id="+id,
-            dataType:"json",
-            success :  function(data){
-                element.parent().remove();
-            }
-        });
-    }
 
     function editBuilding(element){
-        var id = element.parent().children(":first").val();
-        var name= $("#editField").val();
-        var editElement=  element.parent().children("#f-name");
-        $.ajax({
-            type:"POST",
-            data:{"id":id,"name":name},
-            url:"resources/building",
-            dataType:"json",
-            success :  function(data){
-                editElement.children(":first").children().remove().append("<span>"+data.name+"</span>") ;
-                $("#superForm").remove();
-            }
-        });
+
     }
 
 
@@ -82,8 +57,8 @@ $(document).ready(function(){
         }
     });
 
-    $(".del-pane").click(delBuilding($(this)));
-    /*$(".del-pane").click(function(e){
+
+    $(".del-pane").click(function(e){
         console.log("on delete : ");
 
         var id = $(this).parent().children(":first").val();
@@ -91,32 +66,55 @@ $(document).ready(function(){
         console.log("on delete : "+id);
         $.ajax({
             type:"DELETE",
-            url:"resources/building?id="+id,
+            url:"resources/building/"+id,
             dataType:"json",
             success :  function(data){
                 currentElement.parent().remove();
             }
         });
-    });   */
-    var currentElem=0;
+    });
     $(".edit-pane").click(function(){
+        var id = $(this).parent().children(":first").val();
         $("body").append(
             "<div id='superForm'>"+
                 "<div id='superCont'>"+
                     "<input id='editField' type='text'/>"+
                     "<input id='confirmEdit' type='button' class='button' value='ok'/>"+
                     "<input id='cancelEdit' type='button' class='button' value='cancel'/>"+
+                    "<input id='hide' type='hidden' value="+$(this).parent()+"/>"+
+                    "<input id='oldId' type='hidden' value="+id+"/>"+
                 "</div>"+
             "</div>"
         );
-        currentElem=$("this");
     });
 
     $("#cancelEdit").click(function(){
         $("#superForm").remove();
     });
 
-    $("#confirmEdit").click(editBuilding(currentElem));
+    $("#confirmEdit").click(function(){
+        var old=$(this).parent().children("#hide").val();
+        var id=$(this).parent().children("#oldId").val();
+        var newName=$("#editField").val();
+        $.ajax({
+            type:"POST",
+            data:{"id":id,"name":newName},
+            url:"resources/building",
+            dataType:"json",
+            success :  function(data){
+                old.remove();
+                $(".add").before("<div class='entity'>"+
+                "<div class='del-pane' >" +
+                "<input type='hidden' value="+id +" /> &#10006;"+
+                "</div>"+
+                "<div id='f-name'><a href='rooms.html'><span>"+newName+"</span></a></div>" +
+                " <div id='edit-pane' class='active'>" +
+                "<a href=#edit' >&#9998;edit</a>"+
+                "</div></div>");
+                $("#superForm").remove();
+            }
+        });
+    });
 
 
 });
